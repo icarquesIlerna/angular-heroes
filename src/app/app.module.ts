bd20d646174ba0a70117fa5d237533b90397fdcc
icarquesIlerna <icarques@ilernaonline.com>
2019-09-03 16:58:02 +0200

    HeroService, MessageService and MessagesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
-  providers: [],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
